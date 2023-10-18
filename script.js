057e1b782079c3d1e3f68237a3e570cba7694585
Blanca Ziems <blanca481@hotmail.de>
2023-10-18 14:06:05 +0200

    add coding-task: operators and data types

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ console.log(2 != 2);
 console.log(1 >= 1);
 console.log(3 > 3);
 console.log(8 <= 8);
+
+// Coding Task: Use operators & data types
+// Write a string that logs out your first name
+console.log("B" + "l" + "a" + "n" + "c" + "a");
+
+// Spot the problem and solve them
+console.log("Hallo");
+console.log("That doesn't work");
+console.log(50 + 2 >= 52);
+console.log("333" === 333);
+console.log(600 + 510 / 2);
+console.log(2 + 2);
