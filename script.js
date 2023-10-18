11d421ce6854240dccf28841046179c3f12a93d3
Blanca Ziems <blanca481@hotmail.de>
2023-10-18 20:59:26 +0200

    add coding task: first contact with functions

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ console.log(50 + 2 >= 52);
 console.log("333" === 333);
 console.log(600 + 510 / 2);
 console.log(2 + 2);
+
+// Coding Task: First contact with functions
+// Greet function
+function welcomeMsg(name) {
+  return "Welcome " + name;
+}
+console.log(welcomeMsg("Jane"));
+
+// Gross Price Function
+function calcGrossPrice(netPrice, taxRate) {
+  //  return netPrice * ((1 + taxRate) / 1);
+  return Math.round(netPrice * (1 + taxRate) * 100) / 100;
+}
+console.log(calcGrossPrice(20, 0.19));
+console.log(calcGrossPrice(40, 0.16));
+
+// Add Positive Function
+function addPositive(nmbr1, nmbr2) {
+  return Math.abs(nmbr1) + Math.abs(nmbr2);
+}
+console.log(addPositive(2, 3));
+console.log(addPositive(3, -5));
+console.log(addPositive(-1, -8));
