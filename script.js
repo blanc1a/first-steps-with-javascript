ce62c9042a2f0bc40c2b464c8c22187ed99c9ed7
Blanca Ziems <blanca481@hotmail.de>
2023-10-18 13:33:48 +0200

    add Comparison Operators

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ console.log(1 + 1);
 console.log(8 - 5);
 console.log(9 * 9);
 console.log(100 / 5);
+
+// Comparison Operators
+console.log(1 == 1);
+console.log(2 != 1);
+console.log(2 != 2);
+console.log(1 >= 1);
+console.log(3 > 3);
+console.log(8 <= 8);
